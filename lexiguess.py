db3df696eeec8bda92a38ee189ca8c8bcefa0020
tranb7 <tranb7@wwu.edu>
2017-10-16 14:48:35 -0700

    Added comments and fixed up code according to pylint

--- lexiguess.py
+++ lexiguess.py
@@ -1,117 +1,131 @@
 #!/usr/bin/python           # This is server.py file
+"""This runs a client and server for a game of hangman over a network. Clients
+ get 3 guesses to guess the provided word"""
 import socket               # Import socket module
 import argparse
-import random
 import string
 import struct
 import os
 import sys
 
 
-def server(port, word, ip):
-    word, wordLen = hangman(word)
+
+def server(port, word, ipnum):
+    """This creates the server for the game"""
+# creates socket to accept connections
+    wordlen = len(word)
     guesses = 3
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname() # Get local machine name
-    blankW = '_' * wordLen
-    blankW = bytes(blankW, 'utf-8')
-    package = struct.pack('!bb8s', wordLen, guesses, blankW)
-    blankW = blankW.decode("utf-8")
-    if ip != None:
-        s.bind((ip, port))        # Bind to the port
-    else:
-        s.bind((host, port))
-    s.listen(10)                 # Now wait for client connection.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    blankw = '_' * wordlen
+    blankw = bytes(blankw, 'utf-8')
+    package = struct.pack('!b8s', guesses, blankw)
+    blankw = blankw.decode("utf-8")
+    if ipnum is None:
+        ipnum = socket.gethostname()
+    sock.bind((ipnum, port))
+    sock.listen(10)                 # Now wait for client connection.
     i = 1
     while i <= 10:
-        c, addr = s.accept()     # Establish connection with client. This where
+        # allows up to 10 connections to game
+        con, _addr = sock.accept()
         cid = os.fork()
         if cid == 0:
-            send(c, package)
-            while blankW != word and guesses > 0:
-                guess = recv(c)
+            send(con, package)
+            # accepts guesses and sends updated info to client until word is
+            # guessed or player runs out of guesses
+            while blankw != word and guesses > 0:
+                guess = recv(con)
                 guess = struct.unpack('!s', guess)
                 guess = list(guess[0].decode("utf-8"))
-                if guess[0] in word:
-                    index = [pos for pos, char in enumerate(word) if char == guess[0]]
-                    listWord = list(blankW)
+                if guess[0] in word and guess[0] not in blankw:
+                    index = [pos for pos, char in enumerate(word)
+                             if char == guess[0]]
+                    listword = list(blankw)
                     for i in index:
-                        listWord[i] = guess[0]
-                    blankW = ''.join(listWord)
+                        listword[i] = guess[0]
+                    blankw = ''.join(listword)
                 else:
-                    guesses-=1
-                blankW = bytes(blankW, 'utf-8')
-                package = struct.pack('!bb8s', wordLen, guesses, blankW)
-                send(c, package)
-                blankW = blankW.decode("utf-8")
-            c.close()                # Close the connection
+                    guesses -= 1
+                blankw = bytes(blankw, 'utf-8')
+                package = struct.pack('!b8s', guesses, blankw)
+                send(con, package)
+                blankw = blankw.decode("utf-8")
+            con.close()                # Close the connection
         else:
             i += 1
-    s.close() # Closing server socketindex =
+    sock.close()
 
 
-def client(port, ip):
-    s = socket.socket()  # Create a socket object
-    host = socket.gethostname()  # Get local machine name
-    s.connect((ip, port))
-    startWord = recv(s)  # this is where client (or server) waits, WAITALL blocks
-    wordLen, guesses, gameWord = struct.unpack('!bb8s', startWord)
-    gameWord = gameWord.decode("utf-8")
-    while guesses > 0 and '_' in gameWord:
-        print ('Board: ', gameWord, '(',guesses, 'guesses left )')
+def client(port, ipnum):
+    """creates the client to play the game"""
+    sock = socket.socket()
+    if ipnum is None:
+        ipnum = socket.gethostname()
+    sock.connect((ipnum, port))
+    startword = recv(sock)
+    guesses, gameword = struct.unpack('!b8s', startword)
+    gameword = gameword.decode("utf-8")
+    #keeps guessing until player runs out of guesses or word is guessed
+    while guesses > 0 and '_' in gameword:
+        print('Board: ', gameword, '(', guesses, 'guesses left )')
         guess = ''
         while len(guess) != 1:
             guess = input('Enter guess: ')
         guess = guess.lower()
         guess = str.encode(guess)
         message = struct.pack('!s', guess)
-        send(s, message)
-        startWord = recv(s)  # this is where client (or server) waits, WAITALL blocks
-        wordLen, guesses, gameWord = struct.unpack('!bb8s', startWord)
-        gameWord = gameWord.decode("utf-8")
+        send(sock, message)
+        startword = recv(sock)
+        guesses, gameword = struct.unpack('!b8s', startword)
+        gameword = gameword.decode("utf-8")
+    print('Board: ', gameword)
     if guesses > 0:
         print('You won')
     else:
         print('You lost')
-    s.close  # Close the socket when done
-
-
-def hangman(word):
-    if word is None:
-        word = ''
-        for i in range(random.randint(1, 8)):
-            word += (random.choice(string.ascii_lowercase))
-    wordlen = len(word)
-    return word, wordlen
 
 
 def recv(connection):
-    psize = connection.recv(4, socket.MSG_WAITALL)
-    psize = struct.unpack('!i', psize)
-    return connection.recv(psize[0], socket.MSG_WAITALL)
+    """reads size of incoming packet and then reads packet"""
+    connection.settimeout(5)
+    try:
+        psize = connection.recv(4, socket.MSG_WAITALL)
+        psize = struct.unpack('!i', psize)
+        return connection.recv(psize[0], socket.MSG_WAITALL)
+    except TimeoutError:
+        print("Timeout")
 
 
 def send(connection, message):
+    """Sends size of packet and then actual packet"""
     psize = len(message)
     psize = struct.pack('!i', psize)
     connection.send(psize)
     connection.send(message)
 
+
 def main():
+    """Parses command line arguments, starts server or client"""
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument('--mode', choices=['client', 'server'], required=True, help='client or server mode')
+    parser.add_argument(
+        '--mode', choices=['client', 'server'],
+        required=True, help='client or server mode'
+    )
     parser.add_argument('--port', type=int, required=True, help='port number')
     parser.add_argument('--word', help='word to be guessed')
-    parser.add_argument('--ip', help='IP address for client')
+    parser.add_argument('--ip', required=True, help='IP address for client')
     args = parser.parse_args()
     mode = args.mode
     port = args.port
     word = args.word
-    ip = args.ip
+    ipnum = args.ip
     if mode == 'server':
-        server(port, word, ip)
+        if word is None:
+            print("Need to provide word to start game as server")
+            exit()
+        server(port, word, ipnum)
 
     else:
-        client(port, ip)
+        client(port, ipnum)
 
 main()
